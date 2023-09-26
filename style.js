26c039c28db7eb96ee9a5829cb2a55aa3d98ac51
John-thabuks <thabuksjohn@gmail.com>
2023-09-26 17:09:43 +0300

    Added the style.js file and added the auto text generator

---
+++ style.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text",{
+    string:["Frontend Developer", "Backend Developer", "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true
+});
